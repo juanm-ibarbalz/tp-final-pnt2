ecb83f62739310def04b5fc5fc39ede7c24e16db
Parkizan <juanmartin.ibarbalz@gmail.com>
2024-10-28 23:16:07 -0300

    fix getAllFacturas

--- src/services/facturaService.js
+++ src/services/facturaService.js
@@ -1,22 +1,20 @@
 import AWS from "aws-sdk";
+import dotenv from "dotenv";
+dotenv.config();
 
-// Configura el cliente de DynamoDB
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+// Crear el cliente de DynamoDB DocumentClient
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
-/**
- * Obtiene todas las facturas de DynamoDB.
- * @returns {Array} - Una lista con todos los registros de facturas.
- */
-export const getAllFacturas = async () => {
+// Función para obtener todas las facturas
+export async function getAllFacturas() {
   const params = {
     TableName: process.env.DYNAMODB_TABLE_NAME,
   };
 
   try {
-    const data = await dynamoDb.scan(params).promise();
-    return data.Items || [];
+    const data = await dynamoDB.scan(params).promise();
+    return data.Items; // Retorna las facturas obtenidas
   } catch (error) {
     console.error("Error al obtener las facturas:", error);
-    throw new Error(`Error al obtener las facturas: ${error.message}`);
   }
-};
+}
